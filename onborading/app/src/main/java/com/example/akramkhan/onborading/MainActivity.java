44e221ae55369c8001e8863ac4438fe82772c7dc
Akram Khan <mdd.akram@gmail.com>
2016-04-01 12:46:22 +0530

    three different fragments

---
+++ onborading/app/src/main/java/com/example/akramkhan/onborading/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.akramkhan.onborading;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SharedPreferences preferences =  getSharedPreferences("my_preferences", MODE_PRIVATE);
+
+// Check if onboarding_complete is false
+        if(!preferences.getBoolean("onboarding_complete",false)) {
+            // Start the onboarding Activity
+            Intent onboarding = new Intent(this, OnboardingActivity.class);
+            startActivity(onboarding);
+
+            // Close the main Activity
+            finish();
+            return;
+        }
+    }
+}
---
+++ onborading/app/src/main/java/com/example/akramkhan/onborading/OnboardingFragment3.java
@@ -0,0 +1,61 @@
+package com.example.akramkhan.onborading;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
+import com.flaviofaria.kenburnsview.Transition;
+//import com.goka.kenburnsview.KenBurnsView;
+//import com.goka.kenburnsview.LoopViewPager;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Akram Khan on 27-03-2016.
+ */
+public class OnboardingFragment3 extends Fragment {
+    private KenBurnsView mBackgroundImageKbv;
+    private LinearLayout mBackgroundLl;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle s) {
+        View view =  inflater.inflate(
+                R.layout.onboarding_screen3,
+                container,
+                false
+        );
+        KenBurnsView kbv = (KenBurnsView) view.findViewById(R.id.image2);
+        kbv.setTransitionListener(new KenBurnsView.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+            @Override
+            public void onTransitionEnd(Transition transition) {
+
+            }
+        });
+
+        AccelerateDecelerateInterpolator ACCELERATE_DECELERATE = new AccelerateDecelerateInterpolator();
+        RandomTransitionGenerator generator = new RandomTransitionGenerator(8000, ACCELERATE_DECELERATE);
+//duration = 10000ms = 10s and interpolator = ACCELERATE_DECELERATE
+        kbv.setTransitionGenerator(generator); //set new transition on kbv
+
+        return view;
+    }
+
+
+}
